35c871c681aa4002d07f10bd042eaeb63c2a7079
ahmedsalahsorour88 <ahmedsalahsorour88@gmail.com>
2026-08-11 16:25:45 +0300

    fix: change api base url host from 127.0.0.1 to localhost

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static const String serverUrl = 'http://127.0.0.1:8000';
-  static const String baseUrl = 'http://127.0.0.1:8000/api/v1';
+  static const String serverUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://localhost:8000/api/v1';
 
   // Endpoints
   static const String importFiles = '$baseUrl/import-files';
